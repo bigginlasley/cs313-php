94f75e0286d104c67e5046ab9a081f2350b6e350
BigginLasley <bigginlasley@gmail.com>
2019-01-28 17:05:09 -0700

    fixed typo

--- web/index.php
+++ web/index.php
@@ -18,7 +18,7 @@
 
 <div class="d">
   <h3 class="p1">Snowmobiliing</h3>
-  <p>Imagine this, your sitting at the bottom of a mountain looking up at the unmarked snow. You're out in the cold open air. Feeling it sting your nose as you breathe in. Feeling the seld glide over the powder. If you haven't experienced this you're missing out. Snowmobilling is one of my favorite things to do during the winter. Dipping your sled cutting into the powder as you turn up the mountain. Squeezing the throttle, feeling the sled cut deeper as it turns up the hill. Pressing down on your back foot making your sled cut tighter before taking off up the mountain. Unmarked snow, it's yours for the taking. </p>
+  <p>Imagine this, your sitting at the bottom of a mountain looking up at the unmarked snow. You're out in the cold open air. Feeling it sting your nose as you breathe in. Feeling the sled glide over the powder. If you haven't experienced this you're missing out. Snowmobilling is one of my favorite things to do during the winter. Dipping your sled cutting into the powder as you turn up the mountain. Squeezing the throttle, feeling the sled cut deeper as it turns up the hill. Pressing down on your back foot making your sled cut tighter before taking off up the mountain. Unmarked snow, it's yours for the taking. </p>
 </div>
 
 
@@ -28,7 +28,7 @@
 
 <div class="d1">
   <div class="d2">
-  <p>I just wanted to show some pretty cool things about snowmobiling off. It's one of my favortie things to do. The only time I've heard someone having a bad time is when they get cold, and yeah that sucks. As long as you have the proper gear, it's a really fun time.</p>
+  <p>I just wanted to show off some pretty cool things about snowmobiling. It's one of my favortie things to do. The only time I've heard someone having a bad time is when they get cold, and yeah that sucks. As long as you have the proper gear, it's a really fun time.</p>
   </div>
 </div>
 
