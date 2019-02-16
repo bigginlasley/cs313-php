265426c417a3dfb42da0de407af304fe3614250c
Biggin <bigginlasley@gmail.com>
2019-02-16 16:13:54 -0700

    test

--- web/LFG.php
+++ web/LFG.php
@@ -19,6 +19,8 @@ $db = get_db();
         <div class="dropdown-content">
         <a href="person_search.php">By Person</a>
         <a href="activity_search.php">By Activity</a></li>
+        <li class="floatright"><button class="clicker" onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Login</button></li>
+        <li class="floatright"><button class="clicker" onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Sign Up</button></li>
     </ul>
         
     <div class="bg_img-1">
@@ -26,5 +28,101 @@ $db = get_db();
         <span class="border">Welcome<br>  Our mission is to help bring future friends together </span>
         </div>
     </div>    
+
+    <div id="id01" class="modal">
+        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+        <form class="modal-content" action="LFG.php" method="POST">
+          <div class="holder">
+            <h1>Sign Up</h1>
+            <p>Please fill in this form to create an account.</p>
+            <hr>
+
+            <label for="first_name"><b>First Name</b></label>
+            <input type="text" placeholder="Enter First Name" name="first_name" required>
+
+            <label for="last_name"><b>Last Name</b></label>
+            <input type="text" placeholder="Enter Last Name" name="last_name" required>
+
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" required>
+
+            <label for="email"><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="email" required>
+      
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+      
+            <label for="psw-repeat"><b>Repeat Password</b></label>
+            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+            <label for="address"><b>Address</b></label>
+            <input type="text" placeholder="Enter Address" name="address" required>
+
+            <label for="city"><b>City</b></label>
+            <input type="text" placeholder="Enter City" name="city" required>
+
+            <label for="state"><b>State</b></label>
+            <input type="text" placeholder="Enter State" name="state" required>
+
+            <label for="zip"><b>Zip</b></label>
+            <input type="text" placeholder="Enter Zip" name="zip" required>
+
+      
+            <div class="clearfix">
+              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+              <button type="submit" class="signupbtn">Sign Up</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div id="id02" class="modal">
+  
+        <form class="modal-content animate" action="/action_page.php">
+          
+      
+          <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+      
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+              
+            <button type="submit">Login</button>
+            <label>
+              <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+          </div>
+      
+          <div class="container" style="background-color:#f1f1f1">
+            <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cnclbtn">Cancel</button>
+            <span class="psw">Forgot <a href="#">password?</a></span>
+          </div>
+        </form>
+      </div>
+      
+      <script>
+      // Get the modal
+      var modal = document.getElementById('id01');
+      
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+
+      var modal2 = document.getElementById('id02');
+      
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal2) {
+          modal2.style.display = "none";
+        }
+      }
+      </script>
+
+
+
 </body>
 </html>
