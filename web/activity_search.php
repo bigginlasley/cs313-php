46bb82bf630c7d9b45bb2066d973b068d28e0f11
Biggin <bigginlasley@gmail.com>
2019-02-16 16:58:08 -0700

    test

--- web/activity_search.php
+++ web/activity_search.php
@@ -19,14 +19,9 @@ $db = get_db();
 
 <body data-gr-c-s-loaded="true">
 
-<ul>
-        <li><a class="active" href="LFG.php">Home</a></li>
-        <li class="dropdown">
-        <a href="javascript:void(0)" class="dropbtn">Search</a>
-    <div class="dropdown-content">
-      <a href="person_search.php">By Person</a>
-      <a href="activity_search.php">By Activity</a></li>
-    </ul>
+<?php
+require("navbar.php");
+?>
 
 
 <div class="bg_img-1">
