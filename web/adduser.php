fa4c1ca22a31430596e154370de48337b2b3ed31
Biggin <bigginlasley@gmail.com>
2019-02-16 19:10:31 -0700

    test

--- web/adduser.php
+++ web/adduser.php
@@ -10,25 +10,25 @@ $f_name=$_POST['first_name'];
         $l_name=$_POST['last_name'];
         $username=$_POST['username'];
         $email=$_POST['email'];
-        $password=$_POST['password'];
+        $password=$_POST['psw'];
         $address=$_POST['address'];
         $city=$_POST['city'];
         $state=$_POST['state'];
         $zip=$_POST['zip'];
 
         try{
-            $query = 'INSERT INTO person(last_name, first_name, email, usrname, person_address, person_city, person_state, person_zip) VALUES(:last_name, :first_name, :email, :usrname, :person_address, :person_city, :person_state, :person_zip)';
+            $query = 'INSERT INTO person(last_name, first_name, email, usrname, person_address, person_city, person_state, person_zip, password) VALUES(:last_name, :first_name, :email, :usrname, :person_address, :person_city, :person_state, :person_zip, :password)';
             $statement =$db->prepare($query);
 
             $statement->bindValue(':last_name', $l_name);
             $statement->bindValue(':first_name', $f_name);
             $statement->bindValue(':email', $email);
             $statement->bindValue(':usrname', $username);
-            $statement->bindValue(':password', $password);
             $statement->bindValue(':address', $address);
             $statement->bindValue(':city', $city);
             $statement->bindValue(':state', $state);
             $statement->bindValue(':zip', $zip);
+            $statement->bindValue(':password', $password);
 
             $statement->execute();
 
