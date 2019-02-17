4753cd54b20a66fb184fc1df8fc55acc913fae18
Biggin <bigginlasley@gmail.com>
2019-02-16 21:04:45 -0700

    test

--- web/create_activity.php
+++ web/create_activity.php
@@ -15,7 +15,7 @@ $usrname = $_SESSION['username'];
 
 <body data-gr-c-s-loaded="true">
 <div id="id01" class="modal2">
-        <form class="modal2-content" id="sign_up" action="adduser.php" method="POST">
+        <form class="modal2-content" id="sign_up" action="loginhandler.php" method="POST">
           <div class="holder">
             <h1>Create Activity</h1>
             <p>Please fill in this form to create an Activity.</p>
@@ -24,7 +24,7 @@ $usrname = $_SESSION['username'];
             <label for="name"><b>Name of Activity</b></label>
             <input type="text" placeholder="Enter Activity Name" name="name" required>
 
-            <label for="activity">activity</label>
+            <label for="activity">Activity</label>
                        <select id="activity" name="activity">
                          <option value="Lifting">Lifting</option>
                          <option value="Basketball">Basketball</option>
@@ -33,7 +33,7 @@ $usrname = $_SESSION['username'];
                        </select>
 
             <label for="time"><b>Time of Event</b></label>
-            <input type="text" placeholder="Enter Event Tim" name="time" required>
+            <input type="text" placeholder="Enter Event Time" name="time" required>
             
             <label for="address"><b>Address</b></label>
             <input type="text" placeholder="Enter Address" name="address" required>
@@ -42,10 +42,10 @@ $usrname = $_SESSION['username'];
             <input type="text" placeholder="Enter City" name="city" required>
       
             <label for="state"><b>State</b></label>
-            <input type="password" placeholder="Enter State" name="state" required>
+            <input type="text" placeholder="Enter State" name="state" required>
       
             <label for="zip"><b>Zip</b></label>
-            <input type="password" placeholder="Enter Zip" name="zip" required>
+            <input type="text" placeholder="Enter Zip" name="zip" required>
 
             <label for="capacity"><b>Capacity</b></label>
             <input type="text" placeholder="Enter the maximum capacity" name="capacity" required>
