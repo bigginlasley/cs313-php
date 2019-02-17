8f2675e1912e3032372fe7be500362b1def33913
Biggin <bigginlasley@gmail.com>
2019-02-16 18:00:58 -0700

    test

--- web/LFG.php
+++ web/LFG.php
@@ -2,6 +2,13 @@
 session_start();
 require "dbConnect.php";
 $db = get_db();
+
+$_SESSION['loggedin'];
+$f_name=$_POST['first_name'];
+$l_name=$_POST['last_name'];
+
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -15,9 +22,22 @@ $db = get_db();
 
 <body data-gr-c-s-loaded="true">
 
-<?php
-require("navbar.php");
-?>
+<ul>
+        <li><a class="active" href="LFG.php">Home</a></li>
+        <li class="dropdown">
+        <a href="javascript:void(0)" class="dropbtn">Search</a>
+        <div class="dropdown-content">
+        <a href="person_search.php">By Person</a>
+        <a href="activity_search.php">By Activity</a></li>
+
+
+        <?php 
+        
+        ?>
+
+        <li class="floatright"><button class="clicker" onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Login</button></li>
+        <li class="floatright"><button class="clicker" onclick="location.href='LFG.php';">Sign Up</button></li>
+</ul>
 
 
 
@@ -28,56 +48,10 @@ require("navbar.php");
         </div>
     </div>    
 
-    <div id="id01" class="modal">
-        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-        <form class="modal-content" action="LFG.php" method="POST">
-          <div class="holder">
-            <h1>Sign Up</h1>
-            <p>Please fill in this form to create an account.</p>
-            <hr>
-
-            <label for="first_name"><b>First Name</b></label>
-            <input type="text" placeholder="Enter First Name" name="first_name" required>
-
-            <label for="last_name"><b>Last Name</b></label>
-            <input type="text" placeholder="Enter Last Name" name="last_name" required>
-
-            <label for="username"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="username" required>
-
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" required>
-      
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-      
-            <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-
-            <label for="address"><b>Address</b></label>
-            <input type="text" placeholder="Enter Address" name="address" required>
-
-            <label for="city"><b>City</b></label>
-            <input type="text" placeholder="Enter City" name="city" required>
-
-            <label for="state"><b>State</b></label>
-            <input type="text" placeholder="Enter State" name="state" required>
-
-            <label for="zip"><b>Zip</b></label>
-            <input type="text" placeholder="Enter Zip" name="zip" required>
-
-      
-            <div class="clearfix">
-              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-              <button type="submit" class="signupbtn">Sign Up</button>
-            </div>
-          </div>
-        </form>
-      </div>
-
+    
       <div id="id02" class="modal">
   
-      <form class="modal-content animate" action="LFG.php" method="POST">
+      <form class="modal-content animate" id="login" action="LFG.php" method="POST">
           
       
           <div class="holder">
@@ -87,7 +61,7 @@ require("navbar.php");
             <label for="psw"><b>Password</b></label>
             <input type="password" placeholder="Enter Password" name="psw" required>
               
-            <button type="submit">Login</button>
+            <button onclick="submitForm('login')">Login</button>
             <label>
               <input type="checkbox" checked="checked" name="remember"> Remember me
             </label>
@@ -102,14 +76,6 @@ require("navbar.php");
       
       <script>
       // Get the modal
-      var modal = document.getElementById('id01');
-      
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
 
       var modal2 = document.getElementById('id02');
       
---
+++ web/signup.php
@@ -0,0 +1,53 @@
+<?php 
+session_start();
+require "dbConnect.php";
+$db = get_db();
+?>
+
+
+
+<div id="id01" class="modal">
+        <form class="modal-content" id="sign_up" action="LFG.php" method="POST">
+          <div class="holder">
+            <h1>Sign Up</h1>
+            <p>Please fill in this form to create an account.</p>
+            <hr>
+
+            <label for="first_name"><b>First Name</b></label>
+            <input type="text" placeholder="Enter First Name" name="first_name" required>
+
+            <label for="last_name"><b>Last Name</b></label>
+            <input type="text" placeholder="Enter Last Name" name="last_name" required>
+
+            <label for="username"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" required>
+
+            <label for="email"><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="email" required>
+      
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+      
+            <label for="psw-repeat"><b>Repeat Password</b></label>
+            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+            <label for="address"><b>Address</b></label>
+            <input type="text" placeholder="Enter Address" name="address" required>
+
+            <label for="city"><b>City</b></label>
+            <input type="text" placeholder="Enter City" name="city" required>
+
+            <label for="state"><b>State</b></label>
+            <input type="text" placeholder="Enter State" name="state" required>
+
+            <label for="zip"><b>Zip</b></label>
+            <input type="text" placeholder="Enter Zip" name="zip" required>
+
+      
+            <div class="clearfix">
+              <button type="button" onclick="location.href='LFG.php';" class="cancelbtn">Cancel</button>
+              <button onclick="submitForm('sign_up')" class="signupbtn">Sign Up</button>
+            </div>
+          </div>
+        </form>
+      </div>
