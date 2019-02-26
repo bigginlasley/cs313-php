5b24c50e6270d32c96eabefc9781abf5f8381984
Biggin <bigginlasley@gmail.com>
2019-02-25 21:14:01 -0700

    test

--- web/activity_search.php
+++ web/activity_search.php
@@ -43,7 +43,7 @@ $usrname = $_SESSION['username'];
 
 <div class="bg_img-1">
         <div class="d4">
-          <div class="d1">
+          <div class="d5">
           
         <form action="display.php" method="post">
                        
