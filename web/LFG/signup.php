c838cad25a6537fd869a1cb007c28098c25f2ed3
Biggin <bigginlasley@gmail.com>
2019-02-26 21:09:00 -0700

    fixing CSS

--- web/LFG/signup.php
+++ web/LFG/signup.php
@@ -8,7 +8,7 @@ $db = get_db();
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="styler.css" rel="stylesheet" type="text/css"/>
+<link href="../styler.css" rel="stylesheet" type="text/css"/>
 <title>sign up</title>
 </head>
 
